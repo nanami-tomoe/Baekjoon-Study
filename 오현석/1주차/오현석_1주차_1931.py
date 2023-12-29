c987892259de4f4d1d771ac4e7cb5ec760c283f6
5hseok <ohhs1010@gmail.com>
2023-12-29 15:03:49 +0900

    1931 / silver 1 / https://www.acmicpc.net/problem/1931

--- 오현석/1주차/오현석_1주차_1931.py
+++ 오현석/1주차/오현석_1주차_1931.py
@@ -1,24 +1,21 @@
 # 컴알에서 선분 안 겹치게 하기랑 비슷
-def check_time(new, exist):
-    if new[0]==new[1]:
-        exist.update(new[0])
+def check_time(new):
+    global end_time
+    if end_time <= new[0]:
+        end_time = new[1]
         return 1
-    if set(range(new[0],new[1])) & exist:
-        return 0
     else:
-        exist.update(range(new[0],new[1]))
-        return 1
+        return 0
 
 num = int(input())    # 입력받는 횟수
 time_table = []
 for _ in range(num):
     time_table.append(list(map(int,input().split(' '))))
 
-time_table.sort(key=lambda x: (x[1] - x[0]))
+time_table.sort(key=lambda x: (x[1], x[0]))
 current_table = set()
 count = 0
+end_time = 0
 for i in range(num):
-    count += check_time(time_table[i], current_table)
-# print(time_table)
-# print(current_table)
+    count += check_time(time_table[i])
 print(count)
