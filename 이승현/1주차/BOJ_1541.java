c62b65c402f8932fe47358dc065f8fe7f35cf927
nanami-tomoe <npnp123npnp123@gmail.com>
2024-01-04 17:55:27 +0900

    1541 / silver 2 / https://www.acmicpc.net/problem/1541

--- 이승현/1주차/BOJ_1541.java
+++ 이승현/1주차/BOJ_1541.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class BOJ_1541 {
+import java.util.Scanner;
+
+public class BOJ_1541 {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String example = in.nextLine();
+        String[] str = example.split("-");
+
+        for(int i = 0; i < str.length; i++) {
+            int temp = mySum(str[i]);
+            if(i == 0) {
+                answer += temp;
+            } else {
+                answer -= temp;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int mySum(String a) {
+        int sum = 0;
+        String[] str = a.split("[+]"); // +를 인식을 잘 못해서 대괄로 쳐주기
+
+        for(String s : str) {
+            sum += Integer.parseInt(s);
+        }
+
+        return sum;
+    }
 }
