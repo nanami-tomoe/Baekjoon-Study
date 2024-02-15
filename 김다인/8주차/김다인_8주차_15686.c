6363d346b344e895d5fecea6de9639340ec57034
dvin-22 <154444746+dvin-22@users.noreply.github.com>
2024-02-16 01:42:41 +0900

    김다인_8주차_15686.c

--- 김다인/8주차/김다인_8주차_15686.c
+++ 김다인/8주차/김다인_8주차_15686.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+int M, min=9999, N, c=-1, h=-1, city[51][51];
 struct house { int x; int y; };
 struct chicken { int x; int y; };
+struct house H[9999]; struct chicken C[9999], list[9999];
 
 int dis(struct house h, struct chicken c){
   int x=0, y=0;
@@ -10,13 +12,10 @@ int dis(struct house h, struct chicken c){
   else y+=c.y-h.y;
   return x+y;
 }
+void DFS(int s, int L);
 
 int main() {
-  int N, M, c=-1, h=-1;
   scanf("%d %d", &N, &M);
-  struct house H[N*N]; struct chicken C[N*N];
-  int city[N][N];
-
   for (int i=0; i<N; i++){ //도시정보입력&위치저장
     for (int j=0; j<N; j++) {
       scanf("%d", &city[i][j]);
@@ -24,13 +23,29 @@ int main() {
       else if (city[i][j]==2){ H[++h].x=i; H[h].y=j; }
     } }
 
-  //cCm 경우의 수 구현 -> 거리의 합 D[]에 저장
-  
-  /*int D[cCm];  //거리의 합 비교 후 최소값 출력
-  int min=D[0];
-  for (int i=1; i<cCm; i++){
-    if (min>D[i]) min=D[i];
-  }
+  DFS(0, 0);
   printf("%d", min);
-  */
+}
+
+void DFS(int s, int L){
+  if (L == M){
+    for (int i=0; i<M; i++){ //치킨집 선택
+      int D=0;
+      for (int j=0; j<h; j++){ //집에서 가장 까가운 치킨집 찾기
+        int mi=9999;
+        for (int k=0; k<M; k++){
+          int d=dis(H[j], list[k]);
+          if (mi > d) mi=d;
+        }
+        D += mi;
+      }
+      if (min>D) min=D;
+    }
+  }
+  else {
+    for (int i=s; i<c; i++){
+      list[L] = C[i];
+      DFS(i+1, L+1);
+    }
+  }
 }
